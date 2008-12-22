f8c2c0b4c130300ce722758ed579cefeaf1d7a3a
Stephen Haberman <stephen@exigencecorp.com>
2008-12-22 08:52:33 -0600

    Format.

--- src/main/org/exigencecorp/selenify/ResultsServlet.java
+++ src/main/org/exigencecorp/selenify/ResultsServlet.java
@@ -3,7 +3,6 @@ package org.exigencecorp.selenify;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Map;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
@@ -23,86 +22,78 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ResultsServlet implements Servlet {
 
-	public void init(ServletConfig config) {
-	}
+    public void init(ServletConfig config) {
+    }
 
-	public ServletConfig getServletConfig() {
-		return null;
-	}
+    public ServletConfig getServletConfig() {
+        return null;
+    }
 
-	public String getServletInfo() {
-		return null;
-	}
+    public String getServletInfo() {
+        return null;
+    }
 
-	public void destroy() {
-	}
+    public void destroy() {
+    }
 
-	public void service(ServletRequest req, ServletResponse res)
-			throws ServletException, IOException {
-		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) res;
-		response.setContentType("text/html");
-		response.setHeader("Expires", "Sat, 6 May 1995 12:00:00 GMT");
-		response
-				.setHeader("Cache-Control",
-						"no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
-		response.setHeader("Pragma", "no-cache");
-		this.serviceNotCached(request, response);
-	}
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setContentType("text/html");
+        response.setHeader("Expires", "Sat, 6 May 1995 12:00:00 GMT");
+        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+        response.setHeader("Pragma", "no-cache");
+        this.serviceNotCached(request, response);
+    }
 
-	public void serviceNotCached(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		this.saveToXml(request, response);
-		this.saveToTxt(request, response);
-		this.shutdownTheJvmThatWillBringDownJettyAsWell();
-	}
+    public void serviceNotCached(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.saveToXml(request, response);
+        this.saveToTxt(request, response);
+        this.shutdownTheJvmThatWillBringDownJettyAsWell();
+    }
 
-	private void saveToTxt(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		FileWriter fw = new FileWriter(this.getResultsFile("txt"));
-		try {
-			fw.append("result=" + request.getParameter("result"));
-		} finally {
-			fw.close();
-		}
-	}
+    private void saveToTxt(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        FileWriter fw = new FileWriter(this.getResultsFile("txt"));
+        try {
+            fw.append("result=" + request.getParameter("result"));
+        } finally {
+            fw.close();
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	private void saveToXml(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		FileWriter fw = new FileWriter(this.getResultsFile("xml"));
-		try {
-			fw.append(new ResultsParser().toXml(this.getWebapp(),
-					(Map<String, String[]>) request.getParameterMap()));
-		} finally {
-			fw.close();
-		}
-	}
+    @SuppressWarnings("unchecked")
+    private void saveToXml(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        FileWriter fw = new FileWriter(this.getResultsFile("xml"));
+        try {
+            fw.append(new ResultsParser().toXml(this.getWebapp(), request.getParameterMap()));
+        } finally {
+            fw.close();
+        }
+    }
 
-	private File getResultsFile(String extension) {
-		return new File(this.getDirectory() + "/SELENIUM-" + this.getWebapp()
-				+ "-results." + extension);
-	}
+    private File getResultsFile(String extension) {
+        return new File(this.getDirectory() + "/SELENIUM-" + this.getWebapp() + "-results." + extension);
+    }
 
-	private String getWebapp() {
-		return System.getProperty("selenium.results.webapp");
-	}
+    private String getWebapp() {
+        return System.getProperty("selenium.results.webapp");
+    }
 
-	private String getDirectory() {
-		return System.getProperty("selenium.results.directory");
-	}
+    private String getDirectory() {
+        return System.getProperty("selenium.results.directory");
+    }
 
-	private void shutdownTheJvmThatWillBringDownJettyAsWell() {
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					Thread.sleep(1000);
-				} catch (Exception e) {
-					// Ignore
-				}
-				System.exit(0);
-			}
-		}).start();
-	}
+    private void shutdownTheJvmThatWillBringDownJettyAsWell() {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    // Ignore
+                }
+                System.exit(0);
+            }
+        }).start();
+    }
 
 }
